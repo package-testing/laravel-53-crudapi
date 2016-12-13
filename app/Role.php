c4bf120cb70b25e56274e12c42a0347b2f8d8519
PrivateSniper <privatesniper@gmail.com>
2016-12-13 14:16:47 +0000

    Add Roles and User Roles

---
+++ app/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public $timestamps = false;
+
+    public $table = 'roles';
+
+    public $fillable = [
+        'name'
+    ];
+
+    public $validation = [
+        'name' => 'string'
+    ];
+}
